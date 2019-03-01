62d852557d0a79a02db39282a27257cb3694ee1d
Jee Van Kur <40460588+Xhiwankur@users.noreply.github.com>
2019-03-02 02:16:35 +0530

    fixed redundant code

--- code/OpenCV (Vandit)/eye_file.py
+++ code/OpenCV (Vandit)/eye_file.py
@@ -11,8 +11,8 @@ UDP_PORT = 5065
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # xml file location varies from PC to PC
-face_cascade = cv2.CascadeClassifier(r'C:\Users\vandi\PycharmProjects\untitled\venv\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(r'C:\Users\vandi\PycharmProjects\untitled\venv\Lib\site-packages\cv2\data\haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
 
@@ -29,14 +29,8 @@ while 1:
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-            print("X Co-ordinate =")
-            print(ex+(ew/2))
-            print("Y Co-ordinate =")
-            print(ey+(eh/2))
-            print("\n")
-            data = str(ex+(ew/2))
-            sock.sendto(data.encode(), (UDP_IP, UDP_PORT))
-            data = str(ey+(eh /2))
+            print('X=', ex+(ew/2), 'Y=', ey+(eh/2), "\n")
+            data = str(ex+(ew/2)) + 'n' + str(ey+(ey/2))
             sock.sendto(data.encode(), (UDP_IP, UDP_PORT))
 
     cv2.imshow('img', img)
