0da85036400dd5e9fd90fccdef8ff879123559c0
Jee Van Kur <40460588+Xhiwankur@users.noreply.github.com>
2019-03-02 12:18:56 +0530

    state of the art code - Donald Duck

--- code/CV/eye.py
+++ code/CV/eye.py
@@ -19,6 +19,7 @@ cap = cv2.VideoCapture(0)
 while 1:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    height, width, channels = img.shape
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -28,8 +29,8 @@ while 1:
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-            angleX = 2*np.interp(x+w/2, (0, 640), (-30, 30)) #interpolate from -30 to 30 degrees
-            angleY = 2*np.interp(y+h/2, (0, 480), (-30, 30))
+            angleX = 2*np.interp(x+w/2, (0, width), (-30, 30)) #interpolate from -30 to 30 degrees
+            angleY = 2*np.interp(y+h/2, (0, height), (-30, 30))
             print('X=', angleX, 'Y=', angleY, "\n")    
             data = str(angleX) + 'n' + str(angleY)
             sock.sendto(data.encode(), (UDP_IP, UDP_PORT))
